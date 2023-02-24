728e4cea1a1eb266c2b66f0b3ca86b2c66ec645f
Md Estiak Ahmed <49604260+MdEstiakAhmed@users.noreply.github.com>
2023-02-24 19:37:20 +0600

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+	const gotoHome = () => {
+        /**
+         * @param {String} - path string
+         * @param {Object} - { replaceState: true | false }
+        */
+		goto('/');
+	};
+
+    beforeNavigate((navigation) => {
+        console.log('before navigate',navigation);
+    });
+
+    afterNavigate((navigation) => {
+        console.log('after navigate',navigation);
+    });
+</script>
+
+<button on:click="{gotoHome}">goto home</button>
